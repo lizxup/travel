929194af627e55445a63a0d0a9d4274ac091847d
Uto_peanut <lizxup@163.com>
2024-11-20 20:29:09 +0800

    补充缺失文件

---
+++ vue/src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {path: '/',redirect:'/manager/home'},
+    {
+      path: "/manager",
+      component:()=>import("@/views/Manager.vue"),
+      children:[
+        {path: "home", meta:{name:"系统首页"} , component:()=>import("@/views/manager/Home.vue"),},
+        {path: "admin", meta:{name:"管理员信息"} , component:()=>import("@/views/manager/Admin.vue"),},
+        {path: "person", meta:{name:"个人资料"} , component:()=>import("@/views/manager/Person.vue"),},
+        {path: "password", meta:{name:"修改密码"} , component:()=>import("@/views/manager/Password.vue"),},
+        {path: "notice", meta:{name:"系统公告"} , component:()=>import("@/views/manager/notice.vue"),},
+        ]
+    },
+    {path:'/login',component:()=>import('@/views/login.vue')},
+    {path:'/register',component:()=>import('@/views/register.vue')},
+    {path:'/404',component:()=>import('@/views/404.vue')},
+    {path:'/:pathMatch(.*)',redirect:"/404"},
+
+    {
+      path: "/front",
+      component:()=>import("@/views/Front.vue"),
+      children:[
+        {path: "home", component:()=>import("@/views/front/Home.vue")},
+        {path: "person", component:()=>import("@/views/front/Person.vue"),},
+      ]
+    },
+  ]
+})
+
+export default router
